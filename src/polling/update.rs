779078d6042a02e0e13c0030c587a323a71f3676
Nilirad <gisquerin@gmail.com>
2026-03-31 19:41:03 +0200

    Refactor `update_branches_table`

--- src/polling/update.rs
+++ src/polling/update.rs
@@ -1,29 +1,38 @@
+use futures::{StreamExt, TryFutureExt, stream};
 use sqlx::SqlitePool;
 use tracing::{error, info};
 
-use crate::polling::BranchInfo;
+use crate::{error::AppError, polling::BranchInfo};
 
-pub(super) async fn update_branches_table(pool: &SqlitePool, updates_to_process: Vec<BranchInfo>) {
-    for BranchInfo {
-        branch,
-        latest_hash,
-    } in updates_to_process
-    {
-        info!(
-            "New commit detected for branch {}. Hash: {}",
-            branch.name, latest_hash
-        );
-
-        let query_result = sqlx::query!(
-            "UPDATE branches SET last_commit_hash = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?",
-            latest_hash,
-            branch.id
-        )
-        .execute(pool)
+pub(super) async fn update_branches_table(pool: &SqlitePool, branch_infos: Vec<BranchInfo>) {
+    let query_results = stream::iter(branch_infos)
+        .then(|b_info| write_db(b_info, pool))
+        .collect::<Vec<Result<BranchInfo, AppError>>>()
         .await;
 
-        if let Err(e) = query_result {
-            error!("SQLx error: {e}");
-        }
+    let errs = query_results.iter().filter_map(|res| res.as_ref().err());
+    for e in errs {
+        error!("{e}");
     }
+
+    let query_outcomes = query_results.into_iter().filter_map(|res| res.ok());
+    for outcome in query_outcomes {
+        info!(
+            "New commit detected for branch {}. Hash: {}",
+            outcome.branch.name, outcome.latest_hash
+        );
+    }
+}
+
+async fn write_db(branch_info: BranchInfo, pool: &SqlitePool) -> Result<BranchInfo, AppError> {
+    sqlx::query!(
+        "UPDATE branches SET last_commit_hash = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?",
+        branch_info.latest_hash,
+        branch_info.branch.id
+    )
+    .execute(pool)
+    .map_err(AppError::from)
+    .await?;
+
+    Ok(branch_info)
 }
