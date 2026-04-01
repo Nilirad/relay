41acbecebd0bea159ad2597d1f63608609757f45
Nilirad <gisquerin@gmail.com>
2026-04-01 14:27:27 +0200

    Manage DB errors

--- src/polling/mod.rs
+++ src/polling/mod.rs
@@ -93,16 +93,41 @@ async fn followup_poll(res: Result<(), AppError>, token: &CancellationToken) {
 }
 
 async fn handle_polling_error(error: AppError, token: &CancellationToken) {
+    if let AppError::Sqlx(e) = error {
+        handle_sqlx_error(e, token).await;
+    } else {
+        error!("Unexpected error raised: {error}");
+    }
+}
+
+async fn handle_sqlx_error(error: sqlx::Error, token: &CancellationToken) {
     // TODO: Make retry cooldown configurable.
     const DB_ERROR_COOLDOWN_SECS: u64 = 5 * 60;
 
-    if let AppError::Sqlx(e) = error {
-        error!("{e}");
+    let mut critical = false;
+    match error {
+        sqlx::Error::Database(e) => {
+            if e.is_unique_violation() {
+                warn!("Attempted duplicate insertion of unique value: {e}");
+            } else {
+                critical = true;
+                error!("Database error: {e}");
+            }
+        }
+        sqlx::Error::Io(e) => {
+            critical = true;
+            error!("Database I/O error: {e}");
+        }
+        e => {
+            critical = true;
+            error!("{e}")
+        }
+    }
+
+    if critical {
         tokio::select! {
             _ = tokio::time::sleep(Duration::from_secs(DB_ERROR_COOLDOWN_SECS)) => {}
             _ = token.cancelled() => {}
         }
-    } else {
-        error!("Unexpected error raised: {error}");
     }
 }
