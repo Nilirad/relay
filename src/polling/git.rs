164bf219e9d0103853077312f174b7201a9a78fb
Nilirad <gisquerin@gmail.com>
2026-04-24 12:15:51 +0200

    Add unit tests for `extract_hash`

--- src/polling/git.rs
+++ src/polling/git.rs
@@ -44,3 +44,71 @@ fn extract_hash(
             branch,
         })
 }
+
+#[cfg(test)]
+mod tests {
+    #![allow(
+        clippy::panic,
+        clippy::expect_used,
+        clippy::todo,
+        clippy::unimplemented,
+        clippy::indexing_slicing
+    )]
+
+    use super::*;
+
+    #[test]
+    fn test_extract_hash_success() {
+        let stdout = b"678c4343237127dbadbf1806dd98b2154ffd2ebe\trefs/heads/main\n".to_vec();
+        let repo_url = "https://github.com/Nilirad/relay".to_string();
+        let branch = "main".to_string();
+
+        let result = extract_hash(stdout, repo_url, branch);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), "678c4343237127dbadbf1806dd98b2154ffd2ebe");
+    }
+
+    #[test]
+    fn test_extract_hash_empty_output() {
+        let stdout = b"".to_vec();
+        let repo_url = "https://github.com/Nilirad/relay".to_string();
+        let branch = "main".to_string();
+
+        let result = extract_hash(stdout.clone(), repo_url.clone(), branch.clone());
+
+        let Err(CommitHashError::UnexpectedOutput {
+            stdout: err_stdout,
+            repo_url: err_repo_url,
+            branch: err_branch,
+        }) = result
+        else {
+            panic!("Expected UnexpectedOutput error");
+        };
+
+        assert_eq!(err_stdout, String::from_utf8(stdout).unwrap_or_default());
+        assert_eq!(err_repo_url, repo_url);
+        assert_eq!(err_branch, branch);
+    }
+
+    #[test]
+    fn test_extract_hash_whitespace_only() {
+        let stdout = b"   \n\t ".to_vec();
+        let repo_url = "https://github.com/Nilirad/relay".to_string();
+        let branch = "main".to_string();
+
+        let result = extract_hash(stdout.clone(), repo_url.clone(), branch.clone());
+
+        let Err(CommitHashError::UnexpectedOutput {
+            stdout: err_stdout,
+            repo_url: err_repo_url,
+            branch: err_branch,
+        }) = result
+        else {
+            panic!("Expected UnexpectedOutput error");
+        };
+
+        assert_eq!(err_stdout, String::from_utf8(stdout).unwrap_or_default());
+        assert_eq!(err_repo_url, repo_url);
+        assert_eq!(err_branch, branch);
+    }
+}
